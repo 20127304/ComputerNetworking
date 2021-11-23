53143cf8d250f9a0a43272c8b4c7a899ede8274c
hoangvo1902 <75682667+hoangvo1902@users.noreply.github.com>
2021-11-23 12:31:49 +0700

    Add files via upload

---
+++ common.h
@@ -0,0 +1,11 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <cctype>
+using namespace std;
+
+#endif // !_COMMON_H_
